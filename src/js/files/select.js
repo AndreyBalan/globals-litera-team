9a13950b8fca7dcbcd10579495bd7121a58077f7
Andrii Balan <balan.andrey.93@gmail.com>
2024-07-19 09:49:16 +0300

    fix: markup fix

---
+++ src/js/files/select.js
@@ -0,0 +1,16 @@
+const selectElement = document.getElementById("subject");
+let isOpen = false;
+
+selectElement.addEventListener("mousedown", function () {
+    isOpen = !isOpen;
+    if (isOpen) {
+        this.classList.add("open");
+    } else {
+        this.classList.remove("open");
+    }
+});
+
+selectElement.addEventListener("blur", function () {
+    this.classList.remove("open");
+    isOpen = false;
+});
